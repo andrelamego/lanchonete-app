dc9299a69885c179fc2a1edbcdc2d241e67e5aaa
Bruno-Hiroshi <hiroshixdyu@gmail.com>
2025-11-17 16:00:34 -0300

    Update Cargo.java

--- src/main/resources/fatec/lanchoneteapp/app/Cargo.java
+++ src/main/resources/fatec/lanchoneteapp/app/Cargo.java
@@ -1,3 +1,26 @@
 public class Cargo {
 
+    String nome;
+    long salario;
+    String funcao;
+
+    public String getFuncao() {
+        return funcao;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public long getSalario() {
+        return salario;
+    }
+    public void setDescricao(String funcao) {
+        this.funcao = funcao;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setSalario(long salario) {
+        this.salario = salario;
+    }
+
 }
