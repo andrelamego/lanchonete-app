74dfdeb6d3bc5a6079df4ec93b65de0ba4adb50d
André Lamego <andreolamego@gmail.com>
2025-11-24 01:29:14 -0300

    abre novo módulo application.facade no module-info.java para suporte a operações de fachada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,9 @@ module fatec.lanchoneteapp {
 
     opens fatec.lanchoneteapp.run to javafx.fxml;
     opens fatec.lanchoneteapp.adapters.ui to javafx.fxml;
+    opens fatec.lanchoneteapp.application.facade to javafx.fxml;
+
     exports fatec.lanchoneteapp.run;
     exports fatec.lanchoneteapp.adapters.ui;
+    exports fatec.lanchoneteapp.application.facade;
 }
