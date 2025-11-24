d0d9a4c5f9f271ecc0e9db86ac4c32803740dbae
André Lamego <andreolamego@gmail.com>
2025-11-23 23:59:35 -0300

    refatora layout principal e sublayouts FXML para aprimorar organização e responsividade, adicionando novos componentes e políticas de redimensionamento

--- src/main/java/fatec/lanchoneteapp/run/Main.java
+++ src/main/java/fatec/lanchoneteapp/run/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fatec/lanchoneteapp/run/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Gerenciamento de Lanchonete");
         stage.setScene(scene);
         stage.show();
     }
